bdade41638b64f9cdfbf4dff3b9e9086e95e9a5a
piptouque <63.pthiel@gmail.com>
2021-02-11 15:35:51 +0100

    [Fix]: Sting termination.

--- cmake/SetProjectFlags.cmake
+++ cmake/SetProjectFlags.cmake
@@ -24,32 +24,39 @@ endif ()
 
 if (${IS_GNU} OR ${IS_CLANG})
     if (${${PROJECT_NAME}_OPTIMISATION})
-        set(${PROJECT_NAME}_OPTIMISATION_LEVEL "-O3 ")
-    else ()
-        set(${PROJECT_NAME}_OPTIMISATION_LEVEL "")
+        set(${PROJECT_NAME}_OPTIMISATION_LEVEL "-O3")
     endif ()
     ## -Warray-bounds is clang-only I think?
     set(${PROJECT_NAME}_CXX_FLAGS
-            "-fPIC "
-            "-Werror "
-            "-Wall -pedantic "
-            "-Wextra -Wshadow -Wnon-virtual-dtor  -Warray-bounds "
-            "-Wold-style-cast "
-            "-Wimplicit-fallthrough=5 -Wconversion "
-            "${${PROJECT_NAME}_OPTIMISATION_LEVEL} "
+            "-fPIC"
+            "-Werror"
+            "-Wall"
+            "-pedantic"
+            "-Wextra"
+            "-Wshadow"
+            "-Wnon-virtual-dtor"
+            "-Warray-bounds"
+            "-Wold-style-cast"
+            "-Wimplicit-fallthrough=5"
+            "-Wconversion"
+            ${${PROJECT_NAME}_OPTIMISATION_LEVEL}
             )
 elseif (${IS_MSVC})
     if (${${PROJECT_NAME}_OPTIMISATION})
-        set(${PROJECT_NAME}_OPTIMISATION_LEVEL "/O2 ")
-    else ()
-        set(${PROJECT_NAME}_OPTIMISATION_LEVEL "")
+        set(${PROJECT_NAME}_OPTIMISATION_LEVEL "/O2")
     endif ()
     set(${PROJECT_NAME}_CXX_FLAGS
-            "/EHsc "
-            "/WX "
-            "/W4 /w14640 /permissive "
-            "/w14242 /w14254 /w14263 /w14265 /w14287 "
-            "${${PROJECT_NAME}_OPTIMISATION_LEVEL} "
+            "/EHsc"
+            "/WX"
+            "/W4"
+            "/w14640"
+            "/permissive"
+            "/w14242"
+            "/w14254"
+            "/w14263"
+            "/w14265"
+            "/w14287"
+            ${${PROJECT_NAME}_OPTIMISATION_LEVEL}
             )
 else ()
     message(ERROR "Nope!")
